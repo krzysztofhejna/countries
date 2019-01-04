9debf431efde489dd256773afdcc40d26518f9de
krzysztofhejna <khejna15@wp.pl>
2019-01-04 19:34:35 +0100

    Implement country search function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 'use strict';
-var url = 'https://restcountries.eu/rest/v1/name/';
+var url = 'https://restcountries.eu/rest/v2/name/';
 var countriesList = document.getElementById('countries');
 
 function searchCountries() {
@@ -20,9 +20,20 @@ function showCountriesList(resp) {
   }
 
   resp.forEach(function(item) {
-    var listElement = document.createElement('li');
-    listElement.innerText = item.name;
-    countriesList.appendChild(listElement);
+    var parameters = ['name', 'capital', 'currencies[0].name', 'region'];
+    var parameterNames = ['Capital', 'Currency', 'Region'];
+    var newSearchResult = document.createElement('ul');
+    var searchResultHeader = document.createElement('h2');
+
+    searchResultHeader.innerText = item[parameters[0]];
+    countriesList.appendChild(searchResultHeader).classList.add("countries__name");
+    countriesList.appendChild(newSearchResult).classList.add("countries__element");
+
+    for (var i = 1; i < parameters.length; i++) {
+      var listElement = document.createElement('li');
+      listElement.innerHTML = parameterNames[i-1] + ' : <span class="countries__property-value">' + item[parameters[i]] + '</span>';
+      newSearchResult.appendChild(listElement).classList.add("countries__property");
+    }
   });
 }
 
